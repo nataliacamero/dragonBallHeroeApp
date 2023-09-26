49b28595a26f06c1aa75aa2e9d484a4c3600901c
Natalia Camero Carreño <nataliacameroc@gmail.com>
2023-09-26 21:56:30 +0200

    Initial Commit

---
+++ dragonBallHeroesAp/dragonBallHeroesApApp.swift
@@ -0,0 +1,17 @@
+//
+//  dragonBallHeroesApApp.swift
+//  dragonBallHeroesAp
+//
+//  Created by Natalia Camero on 26/9/23.
+//
+
+import SwiftUI
+
+@main
+struct dragonBallHeroesApApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
